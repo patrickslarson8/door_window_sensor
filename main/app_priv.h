ef30ec3b75be4920fa39b4c422a3380cc60dad1c
patrick <pat.s.larson8@outlook.com>
2024-05-22 20:02:13 -0700

    Initial commit

---
+++ main/app_priv.h
@@ -0,0 +1,119 @@
+/*
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+
+#pragma once
+
+#include <esp_err.h>
+#include <esp_matter.h>
+#include <hal/gpio_types.h>
+#include <iot_button.h>
+
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+#include "esp_openthread_types.h"
+#endif
+
+/** Standard max values (used for remapping attributes) */
+#define STANDARD_BRIGHTNESS 255
+#define STANDARD_HUE 360
+#define STANDARD_SATURATION 255
+#define STANDARD_TEMPERATURE_FACTOR 1000000
+
+/** Matter max values (used for remapping attributes) */
+#define MATTER_BRIGHTNESS 254
+#define MATTER_HUE 254
+#define MATTER_SATURATION 254
+#define MATTER_TEMPERATURE_FACTOR 1000000
+
+/** Default attribute values used during initialization */
+#define DEFAULT_POWER true
+#define DEFAULT_BRIGHTNESS 64
+#define DEFAULT_HUE 128
+#define DEFAULT_SATURATION 254
+
+/** GPIO Pin Assignments for sensors **/
+#define DOOR_GPIO_PIN 2
+#define WINDOW_GPIO_PIN 3
+
+const button_config_t door_btn_cfg = {
+    .type = BUTTON_TYPE_GPIO,
+    .long_press_time = 1000,
+    .short_press_time = 100,
+    .gpio_button_config = {
+        .gpio_num = DOOR_GPIO_PIN,
+        .active_level = 0,
+    },
+};
+
+typedef void *app_driver_handle_t;
+
+/** Initialize the light driver
+ *
+ * This initializes the light driver associated with the selected board.
+ *
+ * @return Handle on success.
+ * @return NULL in case of failure.
+ */
+app_driver_handle_t app_driver_light_init();
+
+/** Initialize the button driver
+ *
+ * This initializes the button driver associated with the selected board.
+ *
+ * @return Handle on success.
+ * @return NULL in case of failure.
+ */
+app_driver_handle_t app_driver_button_init();
+
+app_driver_handle_t app_driver_door_init();
+bool app_driver_get_button_state(app_driver_handle_t handle);
+void app_driver_set_door_opened();
+void app_driver_set_door_closed();
+
+/** Driver Update
+ *
+ * This API should be called to update the driver for the attribute being updated.
+ * This is usually called from the common `app_attribute_update_cb()`.
+ *
+ * @param[in] endpoint_id Endpoint ID of the attribute.
+ * @param[in] cluster_id Cluster ID of the attribute.
+ * @param[in] attribute_id Attribute ID of the attribute.
+ * @param[in] val Pointer to `esp_matter_attr_val_t`. Use appropriate elements as per the value type.
+ *
+ * @return ESP_OK on success.
+ * @return error in case of failure.
+ */
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val);
+
+/** Set defaults for light driver
+ *
+ * Set the attribute drivers to their default values from the created data model.
+ *
+ * @param[in] endpoint_id Endpoint ID of the driver.
+ *
+ * @return ESP_OK on success.
+ * @return error in case of failure.
+ */
+esp_err_t app_driver_light_set_defaults(uint16_t endpoint_id);
+
+#if CHIP_DEVICE_CONFIG_ENABLE_THREAD
+#define ESP_OPENTHREAD_DEFAULT_RADIO_CONFIG()                                           \
+    {                                                                                   \
+        .radio_mode = RADIO_MODE_NATIVE,                                                \
+    }
+
+#define ESP_OPENTHREAD_DEFAULT_HOST_CONFIG()                                            \
+    {                                                                                   \
+        .host_connection_mode = HOST_CONNECTION_MODE_NONE,                              \
+    }
+
+#define ESP_OPENTHREAD_DEFAULT_PORT_CONFIG()                                            \
+    {                                                                                   \
+        .storage_partition_name = "nvs", .netif_queue_size = 10, .task_queue_size = 10, \
+    }
+#endif
